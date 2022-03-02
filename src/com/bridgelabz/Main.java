5c8698f66f62032b5a86dbe2e5ba7d8ed97dffdc
mangesh9903 <mangeshb9903@gmail.com>
2022-03-02 07:38:38 +0530

    first commit

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import com.bridgelabz.snakesandladders.SnakesAndLadders;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SnakesAndLadders snakesAndLadders=new SnakesAndLadders();
+        snakesAndLadders.twoPlayers();
+    }
+}
